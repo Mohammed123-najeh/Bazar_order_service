bfa62b0fea40a20d88469aa9360e4d5df72a2f23
Mohammed Najeh <mohammed.xxmoxx@gmail.com>
2025-11-04 02:20:15 -0800

    feat: setup Lumen application bootstrap with facades and eloquent

---
+++ Bazar-Catlog-Service/Bazar-Order-Service/bootstrap/app.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
+    dirname(__DIR__)
+))->bootstrap();
+
+$app = new Laravel\Lumen\Application(
+    dirname(__DIR__)
+);
+
+$app->withFacades();
+$app->withEloquent();
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->configure('database');
+
+$app->router->group([
+    'namespace' => 'App\Http\Controllers',
+], function ($router) {
+    require __DIR__.'/../routes/web.php';
+});
+
+return $app;
+
