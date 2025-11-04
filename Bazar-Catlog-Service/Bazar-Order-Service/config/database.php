83a735939857f907941abd0795b0d7ee38dd4022
Mohammed Najeh <mohammed.xxmoxx@gmail.com>
2025-11-04 02:19:57 -0800

    feat: add database configuration for SQLite

---
+++ Bazar-Catlog-Service/Bazar-Order-Service/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'sqlite'),
+    
+    'connections' => [
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', '/app/orders.db'),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+    ],
+];
+
