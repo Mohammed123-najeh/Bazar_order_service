29985faba0bd86a254da4ce52cde9472f032570e
Mohammed Najeh <mohammed.xxmoxx@gmail.com>
2025-11-04 02:20:12 -0800

    feat: configure exception handler with debug mode support

---
+++ Bazar-Catlog-Service/Bazar-Order-Service/app/Exceptions/Handler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
+    ];
+
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        // Enable detailed error messages for debugging
+        if (env('APP_DEBUG', false)) {
+            return response()->json([
+                'message' => $exception->getMessage(),
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+                'trace' => $exception->getTraceAsString()
+            ], 500);
+        }
+        
+        return parent::render($request, $exception);
+    }
+}
+
